177719bd0751b1ae3e98c0c5307e31542926c4e7
Error404Hong <hongjx0321@gmail.com>
2026-02-26 23:50:00 +0800

    Updated project configuration and prepared backend

--- careerladder_web/career_ladder/app/(main)/home/page.tsx
+++ careerladder_web/career_ladder/app/(main)/home/page.tsx
@@ -1,7 +1,16 @@
-export default function HomePage() {
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function HomePage() {
+    const user = await currentUser();
+    const firstname = user?.firstName;
+    const lastname = user?.lastName;
+    const id = user?.id;
+
     return (
-        <div className="min-h-[95vh] bg-pink-200 flex items-center justify-center">
-            112211
+        <div className="min-h-[95vh] bg-pink-200 flex items-center justify-center flex-col">
+            <div>username - {firstname} {lastname}</div>
+
+            <div> id - {id}</div>
         </div>
     );
 }
